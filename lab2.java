6889324935bab6ab3d306b0952b1d4b5c7f34b82
Elena-Petrikevich <163896657+Elena-Petrikevich@users.noreply.github.com>
2024-10-22 12:40:34 +0600

    Add files via upload

---
+++ lab2.java
@@ -0,0 +1,17 @@
+import java.util.regex.*;
+import java.util.Scanner;
+public class lab2 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите строку с шестнадцатиричным идентификатором цвета в HTML.");
+        String s =sc.nextLine();
+        String regex = "()#((\\d|[a-f]|[A-Z]){6})" ;
+        Pattern p = Pattern. compile (regex);
+        Matcher m = p.matcher(s);
+        while (m.find()) {
+            System.out.println("Идентификатор цвета: " + m.group());
+
+        }
+    }
+}
